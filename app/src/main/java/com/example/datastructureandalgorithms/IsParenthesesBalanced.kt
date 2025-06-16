f502f087707a8199695b21049ec494fe87b63ddc
Ishak Erdogan <23ishak@Ishaks-MBP.lan>
2025-06-16 10:00:41 -0400

    .

---
+++ app/src/main/java/com/example/datastructureandalgorithms/IsParenthesesBalanced.kt
@@ -0,0 +1,33 @@
+package com.example.datastructureandalgorithms
+
+fun isParenthesesBalanced(expression: String): Boolean {
+    val stack = ArrayDeque<Char>()
+
+    for (ch in expression) {
+        when (ch) {
+            '(', '[', '{' -> {
+                stack.addFirst(ch)
+            }
+
+
+            ')', ']', '}' -> {
+                if (stack.isEmpty()) return false
+                val t = stack.removeFirst()
+                if ((ch == ')' && t != '(') ||
+                    (ch == ']' && t != '[') ||
+                    (ch == '}' && t != '{')) {
+                    return false
+                }
+            }
+        }
+    }
+    return stack.isEmpty()
+}
+
+fun main() {
+    val i1 = "((a+b)*c)"
+    val i2 = "(a+b]*c)"
+
+    println(isParenthesesBalanced(i1))
+    println(isParenthesesBalanced(i2))
+}
